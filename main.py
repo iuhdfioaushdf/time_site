38d2a71915500665fafd0cdfb5ca3385b1193e2a
iuhdfioaushdf <118126073+iuhdfioaushdf@users.noreply.github.com>
2024-10-06 18:44:44 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import time
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    time_obj = str(time.time())
+
+    return time_obj
+
+
+if __name__ == '__main__':
+
+    with app.app_context():
+        port = int(os.environ.get("PORT", 8800))
+        app.run(host='0.0.0.0', port=port, debug=True)
